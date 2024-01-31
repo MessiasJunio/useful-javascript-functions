5cad2be665ec8e09ec4a2a267d2f9b7a31221c6b
messiasjunio <messiasjunio1998@gmail.com>
2024-01-30 21:17:05 -0400

    Create flatten function

---
+++ flatten/flatten.js
@@ -0,0 +1,54 @@
+function flatten(value) {
+  if (typeof value !== "object" || value === null) {
+    return value;
+  }
+
+  if (Array.isArray(value)) {
+    return flattenArray(value);
+  }
+
+  return flattenObject(value);
+}
+
+function flattenArray(array) {
+  return array.reduce((acc, curr) => acc.concat(flatten(curr)), []);
+}
+
+function flattenObject(object) {
+  const flattenedObj = {};
+
+  for (const [key, value] of Object.entries(object)) {
+    const valueIsObject =
+      typeof value === "object" && value !== null && !Array.isArray(value);
+
+    const flattenedValue = flatten(value);
+
+    if (valueIsObject) {
+      Object.assign(flattenedObj, flattenedValue);
+    } else {
+      flattenedObj[key] = flattenedValue;
+    }
+  }
+
+  return flattenedObj;
+}
+
+const numbersArray = [1, [3, 45, [5, 3]]];
+console.log("Array:", flatten(numbersArray));
+
+const animalsObj = {
+  animal: {
+    felidae: {
+      cats: {
+        catBreeds: ["Siamese", "Bengal"],
+      },
+      lions: {
+        lionBreeds: ["Asiatic", "Transvall"],
+      },
+    },
+    dogs: {
+      dogBreeds: ["Pitbull", "Poodle"],
+    },
+  },
+};
+console.log("Object:", flatten(animalsObj));
